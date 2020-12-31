cc9d60a396975b2d2b0626baa546cdad1a843d18
viethung7899 <viethung07081999@gmail.com>
2020-12-30 17:00:48 -0800

    Configure the database

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,14 +1,27 @@
 const knex = require('knex');
 
-const pg = knex({
-  client: 'pg',
-  connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'polly',
+const config = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+    },
   },
-});
+
+  production: {
+    client: 'pg',
+    connection: process.env.POSTGRES_URL,
+  },
+};
+
+const pg = knex(
+  process.env.NODE_ENV === 'prouduction'
+    ? config.production
+    : config.development
+);
 
 module.exports = pg;
